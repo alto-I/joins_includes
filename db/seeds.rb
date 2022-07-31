6f6424c75cb8d6df6be2abdb1d13c35226819551
alto-I <blmaldiy@gmail.com>
2022-07-31 12:09:31 +0900

    seedデータの作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+p "seedデータ作成開始"
+
+10.times do |n|
+  user = User.create!(
+    name: "user#{n}",
+    email: "user#{n}@example.com"
+  )
+
+  10.times do |m|
+    post = Post.create!(
+      title: "title#{m} by user#{n}",
+      description: "description#{m}",
+      user_id: user.id
+    )
+
+    10.times do |l|
+      Comment.create!(
+        text: "comment#{l} of post#{m} by user#{n}",
+        post_id: post.id
+      )
+    end
+  end
+end
+
+p "seedデータ作成終了"
